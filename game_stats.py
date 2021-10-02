5ee56f0190d238e33cc92f828d6f08936a644f62
Santosh <existentialist666@gmail.com>
2021-10-02 22:54:06 +0530

    Added conditions to stop the game

--- game_stats.py
+++ game_stats.py
@@ -6,7 +6,12 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # Start alien invasion in an active state.
+        self.game_active = True
+
     def reset_stats(self):
         """Initialize Statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
+
+
 
