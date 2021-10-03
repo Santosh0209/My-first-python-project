0b448bf7adec036e227ad5c86b44ca345ef6bc4d
Santosh <existentialist666@gmail.com>
2021-10-03 16:21:45 +0530

    Add scoring system and finished final draft

--- settings.py
+++ settings.py
@@ -1,18 +1,18 @@
 class Settings:
-    """A class to store all settings for Alien Invasion"""
+    """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's static setting"""
+        """Initialize the game's static settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        # ship setting
+        # Ship settings
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_width = 3
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
@@ -22,20 +22,27 @@ class Settings:
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
-        self.initialize_dynamic_setting()
+        self.initialize_dynamic_settings()
 
-    def initialize_dynamic_setting(self):
-        # Initialize the setting that changes throughout the game.
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
-        self.bullet_speed = 1.5
+        self.bullet_speed = 3.0
         self.alien_speed = 1.0
 
-        # Fleet direction of 1 represent right; -1 represent left.
+        # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
+        # Scoring
+        self.alien_points = 50
+
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
-        self.alien_speed = self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
